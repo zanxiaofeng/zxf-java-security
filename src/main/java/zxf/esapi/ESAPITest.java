e6fee5b05634ca69e306dcf6013007d70817ae75
davis <zanxiaofeng@163.com>
2023-10-20 11:18:11 +0800

    update

--- src/main/java/zxf/esapi/ESAPITest.java
+++ src/main/java/zxf/esapi/ESAPITest.java
@@ -5,7 +5,19 @@ import org.owasp.esapi.codecs.MySQLCodec;
 
 public class ESAPITest {
     public static void main(String[] args) {
+        testEncoder();
+        testValidator();
+    }
+
+    private static void testEncoder() {
         System.out.println("encodeForSQL: ' or 1=1->" + ESAPI.encoder().encodeForSQL(new MySQLCodec(MySQLCodec.Mode.STANDARD), "' or 1=1"));
         System.out.println("encodeForHTML: &<>->" + ESAPI.encoder().encodeForHTML("&<>"));
+    }
+
+    private static void testValidator(){
+        Boolean result1 = ESAPI.validator().isValidInput("HTTPScheme", "sql", "HTTPScheme", 6, false);
+        System.out.println(result1);
+        Boolean result2 = ESAPI.validator().isValidInput("HTTPScheme", "http", "HTTPScheme", 6, false);
+        System.out.println(result2);
     }
 }
